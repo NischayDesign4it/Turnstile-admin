8f5f96e1480bd3cdfd1594f0d506ec6b9eba8c50
NischayDesigns4it <nischayv@designi4t.com>
2024-05-24 14:16:54 +0530

    initail

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:turnstile_admin/presentation/loginPage.dart';
+import 'package:turnstile_admin/presentation/siteManagement.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
--- lib/presentation/siteManagement.dart
+++ lib/presentation/siteManagement.dart
@@ -5,6 +5,7 @@ import 'package:turnstile_admin/presentation/workerManagement.dart';
 import 'package:turnstile_admin/theme/AppBar.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'dashboardPage.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 
 class siteManagement extends StatefulWidget {
@@ -18,63 +19,133 @@ class _siteManagementState extends State<siteManagement> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CustomAppBar(title: AppLocalizations.of(context)!.sitemanage, backgroundColor: Color(0xff071390),
-        icon: Icons.dashboard, onPressed: (){
-          Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => dashboardPage()));
-
+      appBar: CustomAppBar(
+        title: AppLocalizations.of(context)!.sitemanage,
+        backgroundColor: Color(0xff071390),
+        icon: Icons.dashboard,
+        onPressed: () {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => dashboardPage()));
         },
       ),
       body: ListView(
-          children: [
-            ListTile(
-              leading: Icon(Icons.fiber_manual_record, size: 15),
-              title: Text(AppLocalizations.of(context)!.usermanage,
-                style: TextStyle(fontSize: 20),
-              ),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => workerManagement(),
-                  ),
-                );
-              },
+        children: [
+          ListTile(
+            leading: Icon(Icons.fiber_manual_record, size: 15),
+            title: Text(
+              AppLocalizations.of(context)!.usermanage,
+              style: TextStyle(fontSize: 20),
             ),
-            ListTile(
-              leading: Icon(Icons.fiber_manual_record, size: 15),
-              title: Text(AppLocalizations.of(context)!.assetmanage,
-                style: TextStyle(fontSize: 20),
-              ),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => assetManagement(),
-                  ),
-                );
-              },
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => workerManagement(),
+                ),
+              );
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.fiber_manual_record, size: 15),
+            title: Text(
+              AppLocalizations.of(context)!.assetmanage,
+              style: TextStyle(fontSize: 20),
             ),
-            ListTile(
-              leading: Icon(Icons.fiber_manual_record, size: 15),
-              title: Text(AppLocalizations.of(context)!.docView,
-                style: TextStyle(fontSize: 20),
-              ),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => DocViewScreen(),
-                  ),
-                );
-              },
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => assetManagement(),
+                ),
+              );
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.fiber_manual_record, size: 15),
+            title: Text(
+              AppLocalizations.of(context)!.docView,
+              style: TextStyle(fontSize: 20),
             ),
-            // Add more ListTiles as needed
-          ],
-        ),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => DocViewScreen(),
+                ),
+              );
+            },
+          ),
+          Container(
+            color: Colors.white,
+            padding: EdgeInsets.all(20.0),
+            child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: DataTable(
+                columnSpacing: 15.0,
+                columns: [
+                  DataColumn(label: Text(AppLocalizations.of(context)!.enroll)),
+                  DataColumn(label: Text(AppLocalizations.of(context)!.active)),
+                  DataColumn(label: Text(AppLocalizations.of(context)!.inactive)),
+                  DataColumn(label: Text(AppLocalizations.of(context)!.onsite)),
+                  // DataColumn(label: Text('TimeStamp')),
+                ],
+                rows: [
+                  DataRow(cells: [
+                    DataCell(Text('Emp 1')),
+                    DataCell(Text('Emp 1')),
+                    DataCell(Text('Emp 3')),
+                    DataCell(Text('Emp 1')),
+                    // DataCell(Text('Emp 5')),
 
+                  ]),
+                  DataRow(cells: [
+                    DataCell(Text('Emp 2')),
+                    DataCell(Text('Emp 2')),
+                    DataCell(Text('-')),
+                    DataCell(Text('Emp 2')),
+                    // DataCell(Text('Emp 5')),
+                  ]),
+                  DataRow(cells: [
+                    DataCell(Text('Emp 3')),
+                    DataCell(Text('-')),
+                    DataCell(Text('-')),
+                    DataCell(Text('-')),
+                    // DataCell(Text('Emp 5')),
+                  ]),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            color: Colors.white,
+            padding: EdgeInsets.all(20.0),
+            child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: DataTable(
+                columnSpacing: 15.0,
+                columns: [
+                  DataColumn(label: Text(AppLocalizations.of(context)!.onsite)),
+                  DataColumn(label: Text(AppLocalizations.of(context)!.time)),
+                ],
+                rows: [
+                  DataRow(cells: [
+                    DataCell(Text('Emp 1')),
+                    DataCell(Text('2024-05-23T1:59PM')),
+                  ]),
+                  DataRow(cells: [
+                    DataCell(Text('Emp 2')),
+                    DataCell(Text('2024-05-23T1:59PM')),
+                  ]),
+
+                ],
+              ),
+            ),
+          ),
+
+
+          // Add more ListTiles as needed
+        ],
+      ),
     );
   }
 }
